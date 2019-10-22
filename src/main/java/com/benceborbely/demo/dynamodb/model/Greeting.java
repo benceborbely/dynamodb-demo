b011b1258bdcfa6d09a184b08c0f55ff48988ae2
benceborbely <boben93@gmail.com>
2019-10-23 01:11:56 +0200

    Add some code for checking DynamoDB interaction

--- src/main/java/com/benceborbely/demo/dynamodb/model/Greeting.java
+++ src/main/java/com/benceborbely/demo/dynamodb/model/Greeting.java
@@ -1,10 +1,21 @@
 package com.benceborbely.demo.dynamodb.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "greeting")
 public class Greeting {
 
-    private final String content;
+    @DynamoDBHashKey(attributeName = "content")
+    private String content;
+
+    public Greeting() {}
 
     public Greeting(String content) {
+        this.content = content;
+    }
+
+    public void setContent(String content) {
         this.content = content;
     }
 
