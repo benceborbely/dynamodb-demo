b6d570c796083983e0522547ce6d19b325bc4117
benceborbely <boben93@gmail.com>
2019-10-27 17:34:37 +0100

    Use lombok

--- src/main/java/com/benceborbely/demo/dynamodb/model/MovieRequest.java
+++ src/main/java/com/benceborbely/demo/dynamodb/model/MovieRequest.java
@@ -1,7 +1,10 @@
 package com.benceborbely.demo.dynamodb.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class MovieRequest {
 
     private String title;
@@ -16,51 +19,4 @@ public class MovieRequest {
 
     private List<String> starActors;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public long getLength() {
-        return length;
-    }
-
-    public void setLength(long length) {
-        this.length = length;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public int getAgeLimit() {
-        return ageLimit;
-    }
-
-    public void setAgeLimit(int ageLimit) {
-        this.ageLimit = ageLimit;
-    }
-
-    public List<String> getStarActors() {
-        return starActors;
-    }
-
-    public void setStarActors(List<String> starActors) {
-        this.starActors = starActors;
-    }
 }
